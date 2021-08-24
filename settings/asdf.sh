fd415333152d490bf8085e61c48fa768e3381b26
Marcus Kruse <esurk.sucram@gmail.com>
2021-08-24 09:35:25 +0200

    Update asdf settings

--- settings/asdf.sh
+++ settings/asdf.sh
@@ -1,19 +1,12 @@
 info "run settings/asdf.sh"
 
-ASDF_DIR=$HOME/.asdf
-
-asdf () {
+setup_asdf () {
   cmd "asdf plugin add $1"
   cmd "asdf install $1 latest"
   cmd "asdf global $1 latest"
 }
 
-if [ -d $ASDF_DIR ]
-then
-  asdf 'erlang'
-  asdf 'elixir'
-  asdf 'nodejs'
-  asdf 'ruby'
-else
-  info "asdf is not installed"
-fi
+setup_asdf 'erlang'
+setup_asdf 'elixir'
+setup_asdf 'nodejs'
+setup_asdf 'ruby'
